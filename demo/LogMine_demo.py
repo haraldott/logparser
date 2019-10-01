7c64d355b49a631e858120a00c74ae56da3db445
Harald Ott <>
2019-10-01 15:40:03 +0200

    LogMine and LenMa with variable args

--- demo/LogMine_demo.py
+++ demo/LogMine_demo.py
@@ -2,21 +2,22 @@
 import sys
 sys.path.append('../')
 from logparser import LogMine
+import argparse
 
-long_file = True
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-dir', default='/Users/haraldott/Development/thesis/anomaly_detection_main/data/hdfs/sosp/', type=str)
+    parser.add_argument('-file', default='sorted_half.log', type=str)
+    args = parser.parse_args()
 
-if not long_file:
-    input_dir  = '../logs/HDFS/'  # The input directory of log file
-    log_file = 'HDFS_2k.log'  # The input log file name
-else:
-    input_dir  = '/Users/haraldott/Development/thesis/anomaly_detection_main/data/hdfs/sosp/'  # The input directory of log file
-    log_file = 'sorted_half.log'  # The input log file name
-output_dir = 'LogMine_result/' # The output directory of parsing results
-log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>' # HDFS log format
-levels     = 2 # The levels of hierarchy of patterns
-max_dist   = 0.001 # The maximum distance between any log message in a cluster and the cluster representative
-k          = 1 # The message distance weight (default: 1)
-regex      = []  # Regular expression list for optional preprocessing (default: [])
+    input_dir = args.dir  # The input directory of log file
+    log_file = args.file  # The input log file name
+    output_dir = 'LogMine_result/' # The output directory of parsing results
+    log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>' # HDFS log format
+    levels     = 2 # The levels of hierarchy of patterns
+    max_dist   = 0.001 # The maximum distance between any log message in a cluster and the cluster representative
+    k          = 1 # The message distance weight (default: 1)
+    regex      = []  # Regular expression list for optional preprocessing (default: [])
 
-parser = LogMine.LogParser(input_dir, output_dir, log_format, rex=regex, levels=levels, max_dist=max_dist, k=k)
-parser.parse(log_file)
+    parser = LogMine.LogParser(input_dir, output_dir, log_format, rex=regex, levels=levels, max_dist=max_dist, k=k)
+    parser.parse(log_file)
